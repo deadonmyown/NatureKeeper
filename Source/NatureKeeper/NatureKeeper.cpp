12f3c60eaa5af7a428b4fa8091e4773c904d1e6c
pr1esT <xPr1esT@yandex.ru>
2025-09-14 21:33:30 +0300

    add project files

---
+++ Source/NatureKeeper/NatureKeeper.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NatureKeeper.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NatureKeeper, "NatureKeeper" );
+
+DEFINE_LOG_CATEGORY(LogNatureKeeper)
+ 
